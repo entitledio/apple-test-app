84eacde4c61205a99ddd5414ff375f5287c42d48
Sagar Shah <sgrshah@gmail.com>
2018-08-02 09:57:47 -0400

    Initial Commit

---
+++ IAPTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAPTest
+//
+//  Created by Sagar Shah on 8/2/18.
+//  Copyright © 2018 Entitled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
